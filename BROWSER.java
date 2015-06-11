91c1e31078ddc5d7b3d2271fb5eb81156c1cb39a
sughosh360 <sughosh.kaushik360@gmail.com>
2015-06-11 11:42:56 +0530

    Create BROWSER.java
    Here the JBrowser object is created.A driver class for JBrowser.

---
+++ BROWSER.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class BROWSER {
+
+	public static void main(String[] args) {
+		SBrowser browse = new SBrowser();
+		browse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
